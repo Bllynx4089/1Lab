3d1f903ccefdbe50659d61b49a88bcbf658a5955
Bllynx4089 <stepanym2007@gmail.com>
2026-03-27 09:56:56 +0700

    Нахождение параметров заданной трапеции

--- trapezoid.h
+++ trapezoid.h
@@ -5,7 +5,7 @@
 #include <cmath>
 using namespace std;
 
-double TrapezoidPerimeter(double a, double b, double c, double d); //Ради нее
+double TrapezoidPerimeter(double a, double b, double c, double d); //Функция нахождения периметра
 double TrapezoidSquare(double a, double b, double c, double d); //Функция нахождения площади по формуле Герона для трапеции
 double TrapezoidMidLine(double a, double c); //Функция нахождения средней линией трапеции
 int TrapezoidCheck(double a, double b, double c, double d); //Функция проверка параметров заданной трапеции
